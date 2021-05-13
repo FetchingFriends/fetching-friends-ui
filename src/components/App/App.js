9e279511ecfd18bb7de6b02b6c5192a043fa2c8d
Matt McVey <matt.mcvey49@gmail.com>
2021-05-13 16:47:47 -0600

    setup component file and do some styling on header

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Header from '../Header/Header'
+import { Route, Switch } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className="App">
+    <Header />
+    </div>
+  );
+}
+
+export default App;
