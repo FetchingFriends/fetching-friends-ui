90d59ade0ebc0d4666b7fe0f0be537b71bc6f107
Josh Aragon <josharagon99@gmail.com>
2021-05-14 13:26:01 -0600

    Add second fade in

--- src/PostAFriend/PostAFriend.js
+++ src/PostAFriend/PostAFriend.js
@@ -3,18 +3,22 @@ import './PostAFriend.css'
 
 function PostAFriend() {
   const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [reRenderH1, setReRenderH1] = useState(false);
+  const [reRenderH1, setReRenderH1] = useState('name');
   const questions = [{question: 'What\'s your Friend\'s name?', id:'name'}, {question:'How old are they?', id:'age'}, 'What breed?', 'If you could describe them in a short paragraph?', 'Gender?', 'Neutered?', 'House trained?', 'Upload a few photos so we can find your friend a new home!', 'Good with kids or other friends?']
 
   function nextQuestion(){ 
-    setCurrentQuestion(currentQuestion + 1)
-    setReRenderH1(true)
-    setReRenderH1(false)
+    setReRenderH1('age')
+    renderH1()
   }
+
   function renderH1() {
-    if(reRenderH1 === false) {
+    if(reRenderH1 === 'name') {
       return (
-        <h1>{questions[currentQuestion].question}</h1>
+        <h1 className='fade-in'>{questions[0].question}</h1>
+      )
+    } else if(reRenderH1 === 'age') {
+      return (
+        <h1 className='fade'>{questions[1].question}</h1>
       )
     }
   }
@@ -22,11 +26,11 @@ function PostAFriend() {
   return (
     <main>
       <h1>Find your Friend a new Home</h1>
-      {renderH1()}
-      <div>
-      <input type='text'></input>
-      <button type='submit' onClick={() => nextQuestion()}></button>
+      <div className='question'>
+        {renderH1()}
+        <input type='text' className='input'></input>
       </div>
+      <button type='submit' onClick={() => nextQuestion()}></button>
     </main>
   );
 }
