7ab716bcd61e24b92e5dbe992ab0463ada5569c3
Josh Aragon <josharagon99@gmail.com>
2021-05-15 18:10:52 -0600

    conditionally render inputs

--- src/components/PostAFriend/PostAFriend.js
+++ src/components/PostAFriend/PostAFriend.js
@@ -1,21 +1,63 @@
 import React, { useState } from 'react';
 import './PostAFriend.css'
-import renderQuestion from './renderQuestion.js'
 
 function PostAFriend() {
-  const [reRenderH1, setReRenderH1] = useState('name');
-  const questions = [{ question: 'What\'s your Friend\'s name?', id: 'name' }, { question: 'How old are they?', id: 'age' }, {question:'What breed?', id:'breed'}, {question: 'If you could describe them in a short paragraph?', id:'description'}, {question:'Gender?', id:'gender'}, {question: 'Neutered?', id:'fixed'}, {question: 'House trained?', id:'houseTrained'}, {question:'Upload a few photos so we can find your friend a new home!', id:'photos'}, {question: 'Good with kids or other friends?', id:'goodWith'}]
 
+
+  function renderQuestion() {
+    return (
+      <>
+        <p>{questions[renderQuestionById].question}</p>
+        <div className='input'>
+        {renderInput(questions[renderQuestionById].inputType)}
+        </div>
+      </>
+    )
+  }
+  const [inputValue, setInputValue] = useState('')
+
+  function renderInput(inputType) {
+    if(inputType === 'text') {
+      return (<input className={inputType} type={inputType} value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>)
+    } else if(inputType === 'number') {
+      return (<input className={inputType} type={inputType} value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>)
+    } else if(inputType === 'radio') {
+      const returnOptions = questions[renderQuestionById].options.map(option => {
+        return (
+          <div className={inputType}>
+          <label key={`${option} label`}>{option}</label>
+          <input className={inputType} name={questions[renderQuestionById].id} type={inputType} value={option} key={option} />
+          </div>
+        )
+      })
+      return returnOptions
+    }  else if(inputType === 'checkbox') {
+      const returnOptions = questions[renderQuestionById].options.map(option => {
+        return (
+          <div className={inputType}>
+          <label key={`${option} label`}>{option}</label>
+          <input className={inputType} name={questions[renderQuestionById].id} type={inputType} value={option} key={option} />
+          </div>
+        )
+      })
+      return returnOptions
+    }
+  }
+
+  const [renderQuestionById, setrenderQuestionById] = useState(0);
+  const questions = [{ question: 'What\'s your Friend\'s name?', id: 'name', inputType:'text'}, { question: 'How old are they?', id: 'age', inputType:'number'}, {question:'What breed?', id:'breed', inputType:'text'}, {question: 'If you could describe them in a short paragraph?', id:'description', inputType:'text'}, {question:'Gender?', id:'gender', inputType:'radio', options:['M', 'F']}, {question: 'Neutered?', id:'fixed', inputType:'radio', options:['Yes', 'No']}, {question: 'House trained?', id:'houseTrained', inputType:'radio', options:['Yes', 'No']}, {question:'Upload a few photos so we can find your friend a new home!', id:'photos', inputType:'text'}, {question: 'Good with kids or other friends?', id:'goodWith', inputType:'checkbox', options:['Kids', 'Pets']}]
+  const [newPet, setnewPet] = useState({})
   function nextQuestion() {
-    setReRenderH1(questions[0+1].id)
-    renderQuestion(reRenderH1, questions)
+    setnewPet(...newPet, newPet[questions[renderQuestionById].id] = inputValue)
+    setrenderQuestionById(renderQuestionById + 1)
+    setInputValue('');
   }
 
   return (
     <main>
       <h1>Find your Friend a new Home</h1>
       <div className='question'>
-        {renderQuestion(reRenderH1, questions)}
+        {renderQuestion()}
       </div>
       <button className='submit-question' type='submit' onClick={() => nextQuestion()}>➤</button>
     </main>
