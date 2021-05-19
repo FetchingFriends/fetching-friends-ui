3ba5a48d4714cb2bfd538a533271a6c7f0ecd57d
Josh Aragon <josharagon99@gmail.com>
2021-05-19 15:00:41 -0600

    Require fields to be filled in

--- src/components/PostAFriend/PostAFriend.js
+++ src/components/PostAFriend/PostAFriend.js
@@ -5,16 +5,17 @@ function PostAFriend() {
 
   //state
   const [renderQuestionById, setrenderQuestionById] = useState(0);
-  const questions = [{ question: 'What\'s your Friend\'s name?', id: 'name', inputType:'text'}, { question: 'How old are they?', id: 'age', inputType:'number'}, {question:'What breed?', id:'breed', inputType:'text'}, {question: 'If you could describe them in a short paragraph?', id:'description', inputType:'text'}, {question:'Gender?', id:'gender', inputType:'radio', options:['M', 'F']}, {question: 'Neutered?', id:'fixed', inputType:'radio', options:[true, false]}, {question: 'House trained?', id:'houseTrained', inputType:'radio', options:[true, false]}, {question:'Upload a few photos so we can find your friend a new home!', id:'photos', inputType:'text'}, {question: 'Good with kids or other friends?', id:'goodWith', inputType:'checkbox', options:['Kids', 'Pets']}]
+  const questions = [{ question: 'What\'s your Friend\'s name?', id: 'name', inputType: 'text' }, { question: 'How old are they?', id: 'age', inputType: 'number' }, { question: 'What breed?', id: 'breed', inputType: 'text' }, { question: 'If you could describe them in a short paragraph?', id: 'description', inputType: 'text' }, { question: 'Gender?', id: 'gender', inputType: 'radio', options: ['M', 'F'] }, { question: 'Neutered?', id: 'fixed', inputType: 'radio', options: [true, false] }, { question: 'House trained?', id: 'houseTrained', inputType: 'radio', options: [true, false] }, { question: 'Upload a few photos so we can find your friend a new home!', id: 'photos', inputType: 'text' }, { question: 'Good with kids?', id: 'goodWithKids', inputType: 'radio', options: [true, false]}, { question: 'Good other pets?', id: 'goodWithPets', inputType: 'radio', options: [true, false]}]
   const [newPet, setnewPet] = useState({})
   const [inputValue, setInputValue] = useState('')
+  const [radioSelected, setRadioSelected] = useState(null)
 
   //functions
 
   function handleTrueFalseLabel(option) {
-    if(option === true) {
+    if (option === true) {
       return 'Yes'
-    } else if(option === false) {
+    } else if (option === false) {
       return 'No'
     } else {
       return option
@@ -22,38 +23,33 @@ function PostAFriend() {
   }
 
   function renderQuestion() {
+    if(renderQuestionById <= 9){
     return (
       <>
         <p>{questions[renderQuestionById].question}</p>
         <div className='input'>
-        {renderInput(questions[renderQuestionById].inputType)}
+          {renderInput(questions[renderQuestionById].inputType)}
         </div>
       </>
     )
+    } else {
+      return (
+        <h1>Completed</h1>
+      )
+    }
   }
-  
+
   function renderInput(inputType) {
-    if(inputType === 'text') {
-      return (<input className={inputType} type={inputType} value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>)
-    } else if(inputType === 'number') {
-      return (<input className={inputType} type={inputType} value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>)
-    } else if(inputType === 'radio') {
+    if (inputType === 'text') {
+      return (<input className={inputType} type={inputType} value={inputValue} required onChange={(e) => setInputValue(e.target.value)}></input>)
+    } else if (inputType === 'number') {
+      return (<input className={inputType} type={inputType} value={inputValue} required onChange={(e) => setInputValue(e.target.value)}></input>)
+    } else if (inputType === 'radio') {
       const returnOptions = questions[renderQuestionById].options.map(option => {
         return (
-          <div className={inputType}>
-          <label key={`${option} label`}>{ handleTrueFalseLabel(option)}</label>
-          <input className={inputType} name={questions[renderQuestionById].id} type={inputType} value={option} key={option} onClick={(e) => {setInputValue(e.target.value)}}/>
-          </div>
-        )
-      })
-      return returnOptions
-    }  else if(inputType === 'checkbox') {
-      const returnOptions = questions[renderQuestionById].options.map(option => {
-        return (
-          
-          <div className={inputType}> 
-          <label key={`${option} label`}>{option}</label>
-          <input className={inputType} name={questions[renderQuestionById].id} type={inputType} value={option} key={option} onClick={(e) => {setInputValue(e.target.value)}}/>
+          <div className={inputType} key={option + 'div'}>
+            <label key={`${option} label`}>{handleTrueFalseLabel(option)}</label>
+            <input className={inputType} name={questions[renderQuestionById].id} type={inputType} value={option} key={option} required onClick={(e) => {setInputValue(e.target.value)}} />
           </div>
         )
       })
@@ -61,22 +57,25 @@ function PostAFriend() {
     }
   }
 
-  function nextQuestion(inputQuestion) {
-    console.log(inputQuestion)
-    const updatePet = {...newPet}
-    updatePet[inputQuestion] = inputValue
-    setnewPet(updatePet)
-    setrenderQuestionById(renderQuestionById + 1)
-    setInputValue('');
+  function nextQuestion(inputQuestion, event) {
+    if(renderQuestionById <= 9 && inputValue !== ''){
+      setRadioSelected(null)
+      event.preventDefault()
+      const updatePet = { ...newPet }
+      updatePet[inputQuestion] = inputValue
+      setnewPet(updatePet)
+      setrenderQuestionById(renderQuestionById + 1)
+      setInputValue('');
+    }
   }
 
   return (
     <main>
       <h1>Find your Friend a new Home</h1>
-      <div className='question'>
+      <form className='question'>
         {renderQuestion()}
-      </div>
-      <button className='submit-question' type='submit' onClick={() => nextQuestion(questions[renderQuestionById].id)}>➤</button>
+        <button className='submit-question' type='submit' onClick={(e) => nextQuestion(questions[renderQuestionById].id, e)}>➤</button>
+      </form>
     </main>
   );
 }
