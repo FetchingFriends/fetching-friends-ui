94628b615b308bcd8a56af78f67f4d07e4e78288
Owen Hallgren <owenhallgren@gmail.com>
2021-05-24 16:25:06 -0600

    Added links to UserHome

--- src/components/UserHome/UserHome.js
+++ src/components/UserHome/UserHome.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './UserHome.css'
+import { Link } from 'react-router-dom'
 
 
 const UserHome = () => {
   return (
-    <>
+    <>      
       <div className='user-pets'>
         <section className='my-pets'>
           <h2>My Friends</h2>
@@ -18,9 +19,15 @@ const UserHome = () => {
           <h2>Find New Friend</h2>
           <h3>What are you looking for?</h3>
           <div className='search-buttons'>
-            <button className='dog button'>Dog</button>
+           <Link to={`/all-pets/dogs`} style={{textDecoration:'none'}}>
+            <button className='dog button'>Dog</button> 
+           </Link>
+           <Link to={`/all-pets/cats`} style={{textDecoration:'none'}}>
             <button className='cat button'>Cat</button>
+           </Link>
+           <Link to={`/all-pets/other`} style={{textDecoration:'none'}}>
             <button className='other button'>Other</button>
+           </Link>
           </div>
         </div>
         <div>
